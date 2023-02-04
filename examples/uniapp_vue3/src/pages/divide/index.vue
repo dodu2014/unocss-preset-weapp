26926a1a0d78d04c0d02bc30d8220a333ae6c380
tntgroup <dodu@live.cn>
2023-02-04 12:00:52 +0800

    chore: 配置前缀uno-，并在示例页面中使用前缀样式

--- examples/uniapp_vue3/src/pages/divide/index.vue
+++ examples/uniapp_vue3/src/pages/divide/index.vue
@@ -1,54 +1,54 @@
 <template>
-  <view class="p-2 pb-5 space-y-5">
+  <view class="uno-p-2 uno-pb-5 uno-space-y-5">
     <!-- start: divide-x  -->
-    <view class="text-xl font-bold">
+    <view class="uno-text-xl uno-font-bold">
       divide-x
     </view>
 
-    <view class="flex h-50 divide-x divide-yellow-500:50">
-      <view class="flex-1 center">
+    <view class="uno-flex uno-h-50 uno-divide-x uno-divide-yellow-500:50">
+      <view class="uno-flex-1 uno-center">
         view
       </view>
-      <view class="flex-1 center">
+      <view class="uno-flex-1 uno-center">
         view
       </view>
-      <view class="flex-1 center">
+      <view class="uno-flex-1 uno-center">
         view
       </view>
     </view>
     <!-- end: divide-x  -->
 
     <!-- start  -->
-    <view class="text-xl font-bold ">
+    <view class="uno-text-xl uno-font-bold ">
       divide-y
     </view>
 
-    <view class="divide-y divide-blue">
-      <view class="center">
+    <view class="uno-divide-y uno-divide-blue">
+      <view class="uno-center">
         view
       </view>
-      <view class="center">
+      <view class="uno-center">
         view
       </view>
-      <view class="center">
+      <view class="uno-center">
         view
       </view>
     </view>
     <!-- end  -->
 
     <!-- start  -->
-    <view class="text-xl font-bold">
+    <view class="uno-text-xl uno-font-bold">
       divide-(number)
     </view>
 
-    <view class="divide-y-20 divide-red-500 divide-opacity-50">
-      <view class="center">
+    <view class="uno-divide-y-20 uno-divide-red-500 uno-divide-opacity-50">
+      <view class="uno-center">
         view
       </view>
-      <view class="center">
+      <view class="uno-center">
         view
       </view>
-      <view class="center">
+      <view class="uno-center">
         view
       </view>
     </view>
