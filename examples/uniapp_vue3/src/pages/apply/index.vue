b34f9d048ba7cda535f9bb46aea855e56550d1c1
tnt group <dodu@live.cn>
2023-02-06 11:05:43 +0800

    fix: 升级 unocss-preset-weapp@0.4.0 移除apply示例中无前缀项

--- examples/uniapp_vue3/src/pages/apply/index.vue
+++ examples/uniapp_vue3/src/pages/apply/index.vue
@@ -12,17 +12,17 @@
 
 <style scoped>
 .bg-to-t {
-  @apply center h-200 rounded-md bg-gradient-to-t from-#f39c12/60 via-#2ecc71:80 to-#9b59b6_70 mb-3;
+  /* @apply center h-200 rounded-md bg-gradient-to-t from-#f39c12/60 via-#2ecc71:80 to-#9b59b6_70 mb-3; */
   @apply uno-center uno-h-200 uno-rounded-md uno-bg-gradient-to-t uno-from-#f39c12/60 uno-via-#2ecc71:80 uno-to-#9b59b6_70 uno-mb-3;
 }
 
 .bg-unocss{
-  @apply w-100 h-100 bg-cover mb-3 bg-[url(https://img.cdn.sugarat.top/mdImg/MTY2ODA4OTc3MjcyMg==unocss-icon-gray.svg)] m-auto;
+  /* @apply w-100 h-100 bg-cover mb-3 bg-[url(https://img.cdn.sugarat.top/mdImg/MTY2ODA4OTc3MjcyMg==unocss-icon-gray.svg)] m-auto; */
   @apply uno-w-100 uno-h-100 uno-bg-cover uno-mb-3 uno-bg-[url(https://img.cdn.sugarat.top/mdImg/MTY2ODA4OTc3MjcyMg==unocss-icon-gray.svg)] uno-m-auto;
 }
 
 .bg-to-r{
-  --at-apply: center h-200 rounded-md bg-gradient-to-r from-#f39c12/60 via-#2ecc71:80 to-#9b59b6_70 mb-3
-  --at-apply: uno-center uno-h-200 uno-rounded-md uno-bg-gradient-to-r uno-from-#f39c12/60 uno-via-#2ecc71:80 uno-to-#9b59b6_70 uno-mb-3
+  /* --at-apply: center h-200 rounded-md bg-gradient-to-r from-#f39c12/60 via-#2ecc71:80 to-#9b59b6_70 mb-3; */
+  --at-apply: uno-center uno-h-200 uno-rounded-md uno-bg-gradient-to-r uno-from-#f39c12/60 uno-via-#2ecc71:80 uno-to-#9b59b6_70 uno-mb-3;
 }
 </style>
