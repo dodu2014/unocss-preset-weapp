471bdd81555e21bfacf00f35dc679f0dde7a7671
tntgroup <dodu@live.cn>
2023-02-07 10:24:55 +0800

    chore: 启用非值模式

--- examples/uniapp_vue3/src/pages/attributify-mode/index.vue
+++ examples/uniapp_vue3/src/pages/attributify-mode/index.vue
@@ -9,52 +9,43 @@ export default {
 <template>
   <view>
     <button
-      text="sm green"
-      p="y-2 x-4"
-      m="4"
-      my-attr="y-1 x-2 sm"
+      uno-text="sm green"
+      uno-p="y-2 x-4"
+      uno-m="4"
+      uno-my-attr="y-1 x-2 sm"
     >
       Button
     </button>
 
     <button
-      text-base text-blue
-      py-2 px-4
-      m-4
+      uno-text-base uno-text-blue
+      uno-py-2 uno-px-4
+      uno-m-4
     >
       Button
     </button>
 
-    <button
-      li-text="sm green"
-      li-p="y-2 x-4"
-      li-m="4"
-      li-my-attr="y-1 x-2 sm"
-    >
+    <button uno-border="~ red" uno-m="4">
       Button
     </button>
 
-    <button border="~ red" m="4">
+    <button uno-flex="~ col wrap" uno-class="m4">
       Button
     </button>
 
-    <button flex="~ col wrap" class="m4">
-      Button
-    </button>
-
-    <text text="red" li-text="blue">
+    <text uno-text="red" li-text="blue">
       This conflicts with links' text prop
     </text>
 
-    <view bg="#333:30">
+    <view uno-bg="#333:30">
       111
     </view>
 
-    <view bg="[#11dddd]:80">
+    <view uno-bg="[#11dddd]:80">
       222
     </view>
 
-    <view bg="#2ecc71:50">
+    <view uno-bg="#2ecc71:50">
       333
     </view>
   </view>
